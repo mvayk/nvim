2372d96c0c2d85122cee0e83f3b9f911eabda6de
mvayk <mvayk@mvayk.mvayk>
2026-01-05 16:07:19 +1100

    update

--- lua/mvayk/plugin/smear-cursor.lua
+++ lua/mvayk/plugin/smear-cursor.lua
@@ -1,17 +1,22 @@
-return {
-    "https://github.com/sphamba/smear-cursor.nvim",
-    opts = {
-        smear_insert_mode = true,
-        smear_between_buffers = true,
-        smear_between_neighbour_lines = true,
-        scroll_buffer_space = false,
-        time_interval = 100,
-    },
-    config = function ()
-        require("smear_cursor").setup({
-            stiffness = 0.5,
-            trailing_stiffness = 0.49,
-            never_draw_over_target = false,
-        })
-    end
-}
+local enabled = true
+if enabled then
+    return {
+        "https://github.com/sphamba/smear-cursor.nvim",
+        opts = {
+            smear_insert_mode = true,
+            smear_between_buffers = true,
+            smear_between_neighbour_lines = true,
+            scroll_buffer_space = true,
+            time_interval = 100,
+        },
+        config = function ()
+            require("smear_cursor").setup({
+                stiffness = 0.5,
+                trailing_stiffness = 0.49,
+                never_draw_over_target = false,
+            })
+        end
+    }
+else
+    return { }
+end
