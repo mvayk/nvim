035477261821acbefb17133937e1c408c8b04813
mvayk <mvayk@mvayk.mvayk>
2026-01-19 17:50:44 +1100

    update

--- lua/mvayk/plugin/lsp.lua
+++ lua/mvayk/plugin/lsp.lua
@@ -1,61 +1,140 @@
-local enabled = false
+# big thanks to big baby girl claude
+local enabled = true
 
 if enabled then
     return {
-        --
         { "williamboman/mason.nvim" },
         { "williamboman/mason-lspconfig.nvim" },
-        -- { "neovim/nvim-lspconfig" },
-        --{ "ray-x/lsp_signature.nvim" },
-        { "nvim-treesitter/nvim-treesitter" },
-        { "hrsh7th/nvim-cmp" },
-        { "hrsh7th/cmp-buffer" },
         { "stevearc/conform.nvim" },
         { "nvimdev/lspsaga.nvim" },
         { "hedyhli/outline.nvim" },
-        { "hrsh7th/cmp-path" },
-        { "hrsh7th/cmp-nvim-lsp" },
-        { "hrsh7th/cmp-cmdline" },
-        { "hrsh7th/cmp-emoji" },
-        { "onsails/lspkind.nvim" },
-        { "hrsh7th/cmp-calc" },
-        { "ray-x/cmp-treesitter" },
-        --> { "f3fora/cmp-spell" },
-        { "petertriho/cmp-git" },
-        { "L3MON4D3/LuaSnip" },
         { "lopi-py/luau-lsp.nvim" },
+
+        -- Blink.compat for nvim-cmp sources
+        {
+            "saghen/blink.compat",
+            version = "2.*", -- Use v2.* for blink.cmp v1.*
+            lazy = true,
+            opts = {},
+        },
+
+        -- Blink.cmp and its dependencies
+        {
+            "saghen/blink.cmp",
+            version = "1.*", -- Use stable release
+            dependencies = {
+                "rafamadriz/friendly-snippets", -- Snippet collection
+                "moyiz/blink-emoji.nvim", -- Emoji source
+                "Kaiser-Yang/blink-cmp-git", -- Git source
+                "saghen/blink.compat", -- nvim-cmp compatibility layer
+                "hrsh7th/cmp-calc", -- Calculator source via compat
+            },
+            opts = {
+                keymap = {
+                    preset = "default",
+                    -- Custom keymaps matching your original configuration
+                    ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+                    ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+                    ["<C-j>"] = { "select_next", "fallback" },
+                    ["<C-k>"] = { "select_prev", "fallback" },
+                    ["<Tab>"] = { "select_next", "fallback" },
+                    ["<S-Tab>"] = { "select_prev", "fallback" },
+                    ["<C-e>"] = { "hide", "fallback" },
+                    ["<CR>"] = { "accept", "fallback" },
+                },
+                appearance = {
+                    use_nvim_cmp_as_default = true, -- Use nvim-cmp-like appearance
+                    nerd_font_variant = "mono",
+                },
+
+                completion = {
+                    trigger = {
+                        show_on_keyword = true,
+                        show_on_trigger_character = true,
+                        show_on_insert_on_trigger_character = false,
+                    },
+                    list = {
+                        max_items = 200,
+                    },
+                    accept = {
+                        auto_brackets = {
+                            enabled = true,
+                        },
+                    },
+                    menu = {
+                        max_height = 10,
+                        border = "none",
+                        winblend = 0,
+                        winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+                        -- Add delay to prevent fast event context errors
+                        auto_show_delay_ms = 0,
+                        draw = {
+                            columns = {
+                                { "kind_icon" },
+                                { "label", "label_description", gap = 1 },
+                            },
+                        },
+                    },
+                    documentation = {
+                        auto_show = true,
+                        auto_show_delay_ms = 0,
+                        window = {
+                            border = "none",
+                            winblend = 0,
+                            winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+                            max_width = 60,
+                        },
+                    },
+                },
+
+                sources = {
+                    -- Add all sources to default list
+                    -- Built-in: lsp, path, snippets, buffer
+                    -- Custom: emoji, calc, git
+                    --default = { "lsp", "path", "snippets", "buffer", "emoji", "calc", "git" },
+                    default = { "lsp", "path", "snippets", "buffer"};
+
+                    providers = {
+                        emoji = {
+                            name = "Emoji",
+                            module = "blink-emoji",
+                            score_offset = -5,
+                            opts = {
+                                insert = true, -- Insert emoji instead of name
+                            },
+                        },
+
+                        git = {
+                            name = "Git",
+                            module = "blink-cmp-git",
+                            score_offset = -10,
+                        },
+                    },
+                },
+
+                signature = {
+                    enabled = true,
+                    window = {
+                        border = "none",
+                    },
+                },
+            },
+        },
+
         {
             "neovim/nvim-lspconfig",
             dependencies = {
                 "williamboman/mason.nvim",
                 "williamboman/mason-lspconfig.nvim",
-                "hrsh7th/nvim-cmp",
-                "hrsh7th/cmp-nvim-lsp",
-                "L3MON4D3/LuaSnip",
+                "saghen/blink.cmp",
             },
             config = function()
                 require("mason").setup()
                 require("mason-lspconfig").setup({
-                    ensure_installed = { "lua_ls", "clangd", "ast_grep"},
+                    ensure_installed = { "lua_ls", "clangd", "ast_grep", "nil_ls", "rust_analyzer" },
                     automatic_installation = true,
                     automatic_enable = {
                         exclude = { "luau_lsp" }
-                    },
-                })
-                -- local signature_config = {event="InsertEnter"}
-                -- require("lsp_signature").setup(signature_config)
-
-                require("nvim-treesitter.configs").setup({
-                    ensure_installed = {
-                        "html",
-                        "lua",
-                        "c",
-                        "cpp",
-                        "python",
-                        "rust",
-                    },
-                    highlight = {
-                        enable = true,
                     },
                 })
 
@@ -69,93 +148,47 @@ if enabled then
                 })
 
                 vim.diagnostic.config({
-                    virtual_text = true, -- inline text
+                    virtual_text = true,
                     signs = {
                         text = {
-                            [vim.diagnostic.severity.ERROR] = "",
+                            [vim.diagnostic.severity.ERROR] = "",
                             [vim.diagnostic.severity.WARN]  = "",
-                            [vim.diagnostic.severity.INFO]  = "",
-                            [vim.diagnostic.severity.HINT]  = "",
+                            [vim.diagnostic.severity.INFO]  = "",
+                            [vim.diagnostic.severity.HINT]  = "",
                         },
                     },
-                    underline = true,    -- underline errors/warnings
+                    underline = true,
                     update_in_insert = true,
-                    severity_sort = true,       -- show worst errors first
+                    severity_sort = true,
                     float = {
-                        border = "none", -- <-- make the diagnostic popup single
-                        source = "always",  -- show source
-                        header = "",        -- optional header
-                        prefix = "",        -- optional prefix
+                        border = "none",
+                        source = "always",
+                        header = "",
+                        prefix = "",
                     },
                 })
 
-                --> lsp servers
-                --local capabilities = require("cmp_nvim_lsp").default_capabilities()
+                -- Get blink.cmp capabilities for LSP servers
+                local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-                --> completion menu
-                vim.api.nvim_set_hl(0, "CmpBorder", { link = "NormalFloat" })
-                local lspkind = require("lspkind")
-                local cmp = require("cmp")
-                cmp.setup({
-                    formatting = {
-                        format = lspkind.cmp_format({
-                            mode = "symbol_text",
-                            ellipsis_char = '...',
-                            show_labelDetails = true,
-                        }),
-                    },
-                    mapping = cmp.mapping.preset.insert({
-                        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                        ["<C-j>"] = cmp.mapping.select_next_item(),
-                        ["<C-k>"] = cmp.mapping.select_prev_item(),
-                        ["<Tab>"] = cmp.mapping.select_next_item(),
-                        ["<S-tab>"] = cmp.mapping.select_prev_item(),
-                        ["<C-e>"] = cmp.mapping.abort(),
-                        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                    }),
-                    completion = {
-                        keyword_length = 0,
-                    },
-                    performance = {
-                        debounce = 0,
-                        throttle = 0,
-                        fetching_timeout = 50,
-                    },
-                    snippet = {
-                        expand = function(args)
-                            require("luasnip").lsp_expand(args.body)
-                        end,
-                    },
-                    window = {
-                        completion = cmp.config.window.bordered({
-                            border = "none",
-                            winhighlight = "Normal:NormalFloat,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None",
-                            max_width = 50,
-                            scrollbar = false,
-                            col_offset = 0,
-                        }),
-                        documentation = cmp.config.window.bordered({
-                            border = "none",
-                            winhighlight = "Normal:NormalFloat,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None",
-                            scrollbar = false,
-                            max_width = 60,
-                        }),
-                    },
-                    sources = cmp.config.sources({
-                        { name = "nvim_lsp" },
-                        { name = "luasnip" },
-                        { name = "path" },
-                        { name = "buffer" },
-                        { name = "emoji" },
-                        { name = "calc" },
-                        --> this is really annoying { name = "spell" },
-                        { name = "treesitter" },
-                    }),
-                })
+                -- Setup LSP servers using the new vim.lsp.config API (nvim 0.11+)
+                local mason_lspconfig = require("mason-lspconfig")
+                local installed_servers = mason_lspconfig.get_installed_servers()
+
+                for _, server_name in ipairs(installed_servers) do
+                    if server_name ~= "luau_lsp" then -- Skip luau_lsp as it's configured separately
+                        -- Use the new vim.lsp.config API
+                        vim.lsp.config[server_name] = {
+                            capabilities = capabilities,
+                        }
+
+                        -- Enable the LSP server
+                        vim.lsp.enable(server_name)
+                    end
+                end
             end,
         },
     }
 else
-    return { }
+    return {}
 end
---
+++ lua/mvayk/plugin/lsp_old.lua
@@ -0,0 +1,161 @@
+local enabled = false
+
+if enabled then
+    return {
+        --
+        { "williamboman/mason.nvim" },
+        { "williamboman/mason-lspconfig.nvim" },
+        -- { "neovim/nvim-lspconfig" },
+        --{ "ray-x/lsp_signature.nvim" },
+        { "nvim-treesitter/nvim-treesitter" },
+        { "hrsh7th/nvim-cmp" },
+        { "hrsh7th/cmp-buffer" },
+        { "stevearc/conform.nvim" },
+        { "nvimdev/lspsaga.nvim" },
+        { "hedyhli/outline.nvim" },
+        { "hrsh7th/cmp-path" },
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "hrsh7th/cmp-cmdline" },
+        { "hrsh7th/cmp-emoji" },
+        { "onsails/lspkind.nvim" },
+        { "hrsh7th/cmp-calc" },
+        { "ray-x/cmp-treesitter" },
+        --> { "f3fora/cmp-spell" },
+        { "petertriho/cmp-git" },
+        { "L3MON4D3/LuaSnip" },
+        { "lopi-py/luau-lsp.nvim" },
+        {
+            "neovim/nvim-lspconfig",
+            dependencies = {
+                "williamboman/mason.nvim",
+                "williamboman/mason-lspconfig.nvim",
+                "hrsh7th/nvim-cmp",
+                "hrsh7th/cmp-nvim-lsp",
+                "L3MON4D3/LuaSnip",
+            },
+            config = function()
+                require("mason").setup()
+                require("mason-lspconfig").setup({
+                    ensure_installed = { "lua_ls", "clangd", "ast_grep"},
+                    automatic_installation = true,
+                    automatic_enable = {
+                        exclude = { "luau_lsp" }
+                    },
+                })
+                -- local signature_config = {event="InsertEnter"}
+                -- require("lsp_signature").setup(signature_config)
+
+                require("nvim-treesitter.configs").setup({
+                    ensure_installed = {
+                        "html",
+                        "lua",
+                        "c",
+                        "cpp",
+                        "python",
+                        "rust",
+                    },
+                    highlight = {
+                        enable = true,
+                    },
+                })
+
+                require("luau-lsp").setup({
+                    platform = {
+                        type = "roblox"
+                    },
+                    types = {
+                        roblox_security_level = "PluginSecurity"
+                    },
+                })
+
+                vim.diagnostic.config({
+                    virtual_text = true, -- inline text
+                    signs = {
+                        text = {
+                            [vim.diagnostic.severity.ERROR] = "",
+                            [vim.diagnostic.severity.WARN]  = "",
+                            [vim.diagnostic.severity.INFO]  = "",
+                            [vim.diagnostic.severity.HINT]  = "",
+                        },
+                    },
+                    underline = true,    -- underline errors/warnings
+                    update_in_insert = true,
+                    severity_sort = true,       -- show worst errors first
+                    float = {
+                        border = "none", -- <-- make the diagnostic popup single
+                        source = "always",  -- show source
+                        header = "",        -- optional header
+                        prefix = "",        -- optional prefix
+                    },
+                })
+
+                --> lsp servers
+                --local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+                --> completion menu
+                vim.api.nvim_set_hl(0, "CmpBorder", { link = "NormalFloat" })
+                local lspkind = require("lspkind")
+                local cmp = require("cmp")
+                cmp.setup({
+                    formatting = {
+                        format = lspkind.cmp_format({
+                            mode = "symbol_text",
+                            ellipsis_char = '...',
+                            show_labelDetails = true,
+                        }),
+                    },
+                    mapping = cmp.mapping.preset.insert({
+                        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                        ["<C-j>"] = cmp.mapping.select_next_item(),
+                        ["<C-k>"] = cmp.mapping.select_prev_item(),
+                        ["<Tab>"] = cmp.mapping.select_next_item(),
+                        ["<S-tab>"] = cmp.mapping.select_prev_item(),
+                        ["<C-e>"] = cmp.mapping.abort(),
+                        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    }),
+                    completion = {
+                        keyword_length = 0,
+                    },
+                    performance = {
+                        debounce = 0,
+                        throttle = 0,
+                        fetching_timeout = 50,
+                    },
+                    snippet = {
+                        expand = function(args)
+                            require("luasnip").lsp_expand(args.body)
+                        end,
+                    },
+                    window = {
+                        completion = cmp.config.window.bordered({
+                            border = "none",
+                            winhighlight = "Normal:NormalFloat,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None",
+                            max_width = 50,
+                            scrollbar = false,
+                            col_offset = 0,
+                        }),
+                        documentation = cmp.config.window.bordered({
+                            border = "none",
+                            winhighlight = "Normal:NormalFloat,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None",
+                            scrollbar = false,
+                            max_width = 60,
+                        }),
+                    },
+                    sources = cmp.config.sources({
+                        { name = "nvim_lsp" },
+                        { name = "luasnip" },
+                        { name = "path" },
+                        { name = "buffer" },
+                        { name = "emoji" },
+                        { name = "calc" },
+                        --> this is really annoying { name = "spell" },
+                        { name = "treesitter" },
+                    }),
+                })
+            end,
+        },
+    }
+else
+    return { }
+end
