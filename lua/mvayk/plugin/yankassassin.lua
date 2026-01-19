d34c598e0cf23929f8bb4dbb85e4c8e191fe8823
mvayk <mvayk@mvayk.mvayk>
2026-01-19 18:39:28 +1100

    update

--- lua/mvayk/plugin/yankassassin.lua
+++ lua/mvayk/plugin/yankassassin.lua
@@ -9,7 +9,8 @@ if enabled then
                 auto_visual = true,
             })
             vim.keymap.set({ "x", "n" }, "gy", "<Plug>(YADefault)", { silent = true })
-            vim.keymap.set({ "x", "n" }, "<leader>y", "<Plug>(YANoMove)", { silent = true })
+            --vim.keymap.set({ "x", "n" }, "<leader>y", "<Plug>(YANoMove)", { silent = true })
+            vim.keymap.set({ "x", "n" }, "y", "<Plug>(YANoMove)", { silent = true })
         end,
     }
 else
--- lua/mvayk/set.lua
+++ lua/mvayk/set.lua
@@ -79,7 +79,7 @@ vim.opt.signcolumn     = "yes"
 
 --> lsp hover diagnostics thing <--
 vim.o.updatetime = 250  -- faster CursorHold
-vim.api.nvim_create_autocmd("CursorHold", {
+vim.api.nvim_create_autocmd("cursorhold", {
     callback = function()
         vim.diagnostic.open_float(nil, { focus = false })
     end
