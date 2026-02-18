b66be344e9d2d6fb9d31371ec7dadbb0114cd27c
mvayk <45300955+mvayk@users.noreply.github.com>
2026-02-18 11:28:09 +1100

    update

--- lua/matugen.lua
+++ lua/matugen.lua
@@ -4,18 +4,18 @@ function M.setup()
     require('base16-colorscheme').setup {
         base00 = '#121316', -- Default Background
         base01 = '#1e2022', -- Lighter Background (status bars)
-        base02 = '#292a2d', -- Selection Background
+        base02 = '#282a2d', -- Selection Background
         base03 = '#8d9199', -- Comments, Invisibles
-        base04 = '#c3c6cf', -- Dark Foreground (status bars)
-        base05 = '#e3e2e6', -- Default Foreground
-        base06 = '#e3e2e6', -- Light Foreground
-        base07 = '#e3e2e6', -- Lightest Foreground
+        base04 = '#c3c7cf', -- Dark Foreground (status bars)
+        base05 = '#e2e2e6', -- Default Foreground
+        base06 = '#e2e2e6', -- Light Foreground
+        base07 = '#e2e2e6', -- Lightest Foreground
         base08 = '#ffb4ab', -- Variables, XML Tags, Errors
-        base09 = '#d7bde4', -- Integers, Constants
+        base09 = '#d6bee4', -- Integers, Constants
         base0A = '#bbc7db', -- Classes, Search Background
-        base0B = '#a0c9ff', -- Strings, Diff Inserted
-        base0C = '#d7bde4', -- Regex, Escape Chars
-        base0D = '#a0c9ff', -- Functions, Methods
+        base0B = '#9ecaff', -- Strings, Diff Inserted
+        base0C = '#d6bee4', -- Regex, Escape Chars
+        base0D = '#9ecaff', -- Functions, Methods
         base0E = '#bbc7db', -- Keywords, Storage
         base0F = '#93000a', -- Deprecated, Embedded Tags
     }
