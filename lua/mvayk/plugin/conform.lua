90e1ec87c58e7c3c4c60c68eb1274b080cb05d19
mvayk <45300955+mvayk@users.noreply.github.com>
2026-03-03 20:49:09 +1100

    update

--- lua/mvayk/plugin/conform.lua
+++ lua/mvayk/plugin/conform.lua
@@ -1,70 +1,70 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			"g=",
-			function()
-				require("conform").format({ async = true, lsp_fallback = true })
-			end,
-			desc = "Format buffer",
-		},
-	},
-	config = function()
-		require("conform").setup({
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+        {
+            "=",
+            function()
+                require("conform").format({ async = true, lsp_fallback = true })
+            end,
+            desc = "Format buffer",
+        },
+    },
+    config = function()
+        require("conform").setup({
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_fallback = true,
+            },
 
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "ruff_format", "ruff_organize_imports" },
-				rust = { "rustfmt" },
-				c = { "clang_format" },
-				cpp = { "clang_format" },
-				javascript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescript = { "prettier" },
-				typescriptreact = { "prettier" },
-				html = { "prettier" },
-				css = { "prettier" },
-				scss = { "prettier" },
-				json = { "prettier" },
-				jsonc = { "prettier" },
-				markdown = { "prettier" },
-				yaml = { "prettier" },
-				go = { "goimports", "gofmt" },
-				sh = { "shfmt" },
-				bash = { "shfmt" },
-				zsh = { "shfmt" },
-				nix = { "nixfmt" },
-				toml = { "taplo" },
-				sql = { "sqlfmt" },
-				java = { "google_java_format" },
-				ruby = { "rubocop" },
-				php = { "php_cs_fixer" },
-				zig = { "zigfmt" },
-				dockerfile = {},
+            formatters_by_ft = {
+                lua = { "stylua" },
+                python = { "ruff_format", "ruff_organize_imports" },
+                rust = { "rustfmt" },
+                c = { "clang_format" },
+                cpp = { "clang_format" },
+                javascript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescript = { "prettier" },
+                typescriptreact = { "prettier" },
+                html = { "prettier" },
+                css = { "prettier" },
+                scss = { "prettier" },
+                json = { "prettier" },
+                jsonc = { "prettier" },
+                markdown = { "prettier" },
+                yaml = { "prettier" },
+                go = { "goimports", "gofmt" },
+                sh = { "shfmt" },
+                bash = { "shfmt" },
+                zsh = { "shfmt" },
+                nix = { "nixfmt" },
+                toml = { "taplo" },
+                sql = { "sqlfmt" },
+                java = { "google_java_format" },
+                ruby = { "rubocop" },
+                php = { "php_cs_fixer" },
+                zig = { "zigfmt" },
+                dockerfile = {},
 
-				["_"] = { "trim_whitespace" },
-			},
+                ["_"] = { "trim_whitespace" },
+            },
 
-			formatters = {
-				stylua = {
-					prepend_args = { "--indent-type", "Spaces", "--indent-width", "4" },
-				},
-				shfmt = {
-					prepend_args = { "-i", "4" },
-				},
-				clang_format = {
-					prepend_args = { "--style=file", "--fallback-style=LLVM" },
-				},
-				prettier = {
-					prepend_args = { "--tab-width", "2", "--use-tabs", "false" },
-				},
-			},
-		})
-	end,
+            formatters = {
+                stylua = {
+                    prepend_args = { "--indent-type", "Spaces", "--indent-width", "4" },
+                },
+                shfmt = {
+                    prepend_args = { "-i", "4" },
+                },
+                clang_format = {
+                    prepend_args = { "--style=file", "--fallback-style=LLVM" },
+                },
+                prettier = {
+                    prepend_args = { "--tab-width", "2", "--use-tabs", "false" },
+                },
+            },
+        })
+    end,
 }
