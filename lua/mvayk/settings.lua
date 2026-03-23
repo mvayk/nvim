6556d2bf942a8448bcd5a3a99cede84b80dcc301
mvayk <45300955+mvayk@users.noreply.github.com>
2026-03-24 05:33:24 +1100

    update

--- lua/mvayk/settings.lua
+++ lua/mvayk/settings.lua
@@ -1,7 +1,7 @@
 return {
     border = "none",
     theme_file = vim.fn.stdpath("data") .. "/last_theme.lua",
-    transparent_background = false,
+    transparent_background = true,
     neovide_animations = true,
     --icon_theme_package = "nvim-tree/nvim-web-devicons",
     icon_theme_package = "DaikyXendo/nvim-material-icon",
