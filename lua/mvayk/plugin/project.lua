0240adba47cd12339990f72ce16586b51a1200a9
mvayk <45300955+mvayk@users.noreply.github.com>
2026-05-28 23:48:02 +1000

    update

--- lua/mvayk/plugin/project.lua
+++ lua/mvayk/plugin/project.lua
@@ -7,7 +7,7 @@ return {
         require("project").setup({
             -- your existing options here (e.g. detection_methods, silent_chdir, etc.)
             --detection_methods = { "lsp", "pattern" },
-            patterns = { ".git", "Makefile", "package.json", "flake.nix", "shell.nix", "compile_commands.json"}, -- example
+            patterns = { ".git", "package.json", "flake.nix", "shell.nix", "compile_commands.json"}, -- example
             -- add any other custom opts
             -- enable_autochdir = false,
             -- show_hidden = true,
